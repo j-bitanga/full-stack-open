e9e664ec7376fd1a50c10b556cbcaf603cf3e4dd
j-bitanga <jbitanga88@gmail.com>
2020-10-27 23:21:00 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+    console.log(good)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+    console.log(neutral)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    console.log(bad)
+  }
+
+  return (
+    <div>
+    <h1>Please leave feedback</h1>
+     <Button onClick={handleGood} text='good'/>
+     <Button onClick={handleNeutral} text='neutral'/>
+     <Button onClick={handleBad} text='bad'/>
+     <h2>Stats</h2>
+     Good: {good}<br></br>
+     Neutral: {neutral}<br></br>
+     Bad: {bad}<br></br>
+    Total: {total}<br></br>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
