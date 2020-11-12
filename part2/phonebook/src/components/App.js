af8abbc41fa8f4070a7b20694dc54b63497f41f3
j-bitanga <jbitanga88@gmail.com>
2020-11-11 19:39:35 -0800

    update to phonebook

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -6,12 +6,25 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addName = (event) => {
+      event.preventDefault()
+      console.log('button clicked', event.target)
+  }
+
+  const handleNameChange = (event) => {
+      console.log(event.target.value)
+      setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+                    value={newName}
+                    onChange={handleNameChange}
+                         />
         </div>
         <div>
           <button type="submit">add</button>
